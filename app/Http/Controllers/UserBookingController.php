9578b5abce0446c90b6cfae3325676043ee8503d
Rana Osama <ranaosama27698@gmail.com>
2021-04-24 23:42:03 +0200

    index function for available seats for bus

--- app/Http/Controllers/UserBookingController.php
+++ app/Http/Controllers/UserBookingController.php
@@ -10,6 +10,11 @@ use App\Models\User_booking;
 class UserBookingController extends Controller
 {
     public function store(Request $request){
+        $results = [
+            'success' => false,
+            'data' => [],
+            'message' => ""
+            ];
         $user_id=$request->user_id;
         $start_station_id=$request->start_station;
         $end_station_id=$request->end_station;
@@ -21,10 +26,12 @@ class UserBookingController extends Controller
                 'user_id'=>$user_id,
                 'trip_main_id'=>$trip_main->first()->id
                 ])){
-                    echo "created main";
+                    $results['success']=true;
+                    $results['message']="Bus booked successfully!";
 
             }else{
-                echo "not created";
+                $results['success'] = false;
+                $results['message'] = "Something went wrong, we were not able to book the trip for you.";
             }
         
         }else{
@@ -36,15 +43,66 @@ class UserBookingController extends Controller
                     'trip_main_id'=>$trip_mid->first()->trip_main_id,
                     'trip_mid_id'=>$trip_mid->first()->id
                     ])){
-                        echo "created mid";
+                        $results['success']=true;
+                        $results['message']="Bus booked successfully!";
                 }else{
-                    echo "not created";
+                    $results['success'] = false;
+                    $results['message'] = "Something went wrong, we were not able to book the trip for you.";
                 }
             }else{
-                echo "trip doesn't exist";
+                $results['success'] = false;
+                $results['message'] = "Sorry, Your Trip doesn't exist !";
 
             }
         }
+        return response()->json($results);
 
+    }
+    public function index(Request $request){
+        $results = [
+            'success' => false,
+            'data' => [],
+            'message' => ""
+            ];
+        $start_station_id=$request->start_id;
+        $end_station_id=$request->end_id;
+        $trip_main=TripsMain::where('start_station_id',$start_station_id)
+        ->where('end_station_id',$end_station_id)->get();
+        if($trip_main->count()>0){
+            $trip_id=$trip_main->first()->id;
+            $seats_num=User_booking::where('trip_main_id',$trip_id)->count();
+            $available_seat=12-$seats_num;
+            if($available_seat>0){
+                $results['success'] = true;
+                $results['data']['seats_available']=$available_seat;
+                $results['message'] =  "We have available seats";
+            }else{
+                $results['success'] = false;
+                $results['message'] = "Sorry, Bus Fully Booked.";
+            }
+        }else{
+            $trip_mid=TripsMid::where('start_station_id',$start_station_id)
+            ->where('end_station_id',$end_station_id)->get();
+            if($trip_mid->count()>0){
+                $trip_id=$trip_mid->first()->id;
+                $trip_main_id=$trip_mid->first()->trip_main_id;
+                $seats_num_main=User_booking::where('trip_main_id',$trip_main_id)
+                ->whereNull('trip_mid_id')->count();
+                $seats_num_mid=User_booking::whereHas('TripMid',function($query) use($end_station_id){
+                    $query->where('end_station_id','>=',$end_station_id);
+                })->count();
+                $available_seat=12-($seats_num_main+$seats_num_mid);
+                if($available_seat>0){
+                    $results['success'] = true;
+                    $results['data']['seats_available']=$available_seat;
+                    $results['message'] =  "We have available seats";
+                }else{
+                    $results['success'] = false;
+                    $results['message'] = "Sorry, Bus Fully Booked.";
+                }
+
+            }
+        }
+        return response()->json($results);
     }
 }
--- app/Models/User_booking.php
+++ app/Models/User_booking.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\TripsMid;
 class User_booking extends Model
 {
     
     use HasFactory;
     protected $fillable = ['user_id', 'trip_main_id','trip_mid_id'];
+    public function tripMid(){
+        return $this->belongsTo('App\Models\TripsMid','trip_mid_id','id');
+    }
 }
